216332022dc7eb93f5d0f8b701bd7e8670fc5a4e
umutHatipoglu <umut.hatipoglu@gmail.com>
2018-12-19 15:26:39 +0100

    Implemented move method of HumanPlayer. Implemented the human part of Move.

--- main/java/net/leanix/dev/HumanPlayer.java
+++ main/java/net/leanix/dev/HumanPlayer.java
@@ -1,5 +1,8 @@
 package net.leanix.dev;
 
+import java.util.Scanner;
+import javax.naming.OperationNotSupportedException;
+
 public class HumanPlayer extends BasePlayer implements Player {
 
     public HumanPlayer(String name, CellState symbol) {
@@ -8,6 +11,25 @@ public class HumanPlayer extends BasePlayer implements Player {
 
     @Override
     public void move(Board board) {
+        System.out.println("Please insert cell to put a sign (e.g. A1.).");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
 
+        char column = input.charAt(0);
+        int row = Integer.parseInt(String.valueOf(input.charAt(1)));
+
+        try {
+            Move move = new Move(column, row);
+
+            if (board.attemptMove(this, move.getX(), move.getY())) {
+                System.out.println(getName() + ">" + move.toString());
+            } else {
+                System.out.println(getName() + "> Your input was invalid. Please try again.");
+                move(board);
+            }
+        } catch (OperationNotSupportedException ex) {
+            System.out.println(getName() + "> Your input was invalid. Please try again.");
+            move(board);
+        }
     }
 }
--- main/java/net/leanix/dev/Move.java
+++ main/java/net/leanix/dev/Move.java
@@ -1,5 +1,7 @@
 package net.leanix.dev;
 
+import javax.naming.OperationNotSupportedException;
+
 public class Move {
     private int x;  // row
     private int y;  //column
@@ -7,6 +9,27 @@ public class Move {
     public Move(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    public Move(char column, int row) throws OperationNotSupportedException {
+        this.y = row;
+        this.x = getXMapping(column);
+    }
+
+    private int getXMapping(char c) throws OperationNotSupportedException {
+        switch (c) {
+            case 'A':
+            case 'a':
+                return 0;
+            case 'B':
+            case 'b':
+                return 1;
+            case 'C':
+            case 'c':
+                return 2;
+            default:
+                throw new OperationNotSupportedException("Invalid column input.");
+        }
     }
 
 

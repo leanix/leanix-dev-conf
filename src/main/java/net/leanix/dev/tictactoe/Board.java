a22b883c689cef8a7b48e9bace70f8ef6ba055d1
Soenke Hack <soenke.hack@leanix.net>
2018-12-19 11:08:52 +0100

    Add board

--- src/main/java/net/leanix/dev/tictactoe/Board.java
+++ src/main/java/net/leanix/dev/tictactoe/Board.java
@@ -2,16 +2,30 @@ package net.leanix.dev.tictactoe;
 
 public class Board {
 
-    /**
-     *
-     * @param coordinate
-     * @param state 0 is empty, 1/2 is player number
-     */
-    public void setCellState(Coordinate coordinate, int state) {
+    int[9]state;
+
+    public Board() {
 
     }
 
-    public int getCellState(Coordinate coordinate) {
+    /**
+     *
+     * @param coordinate values
+     * @param state 0 is empty, 1/2 is player number
+     */
+    public void setCellState(Coordinate coordinate, int state) {
+        if (state < 0 || state > 2) {
+            
+        }
+        int x = coordinate.getX();
+        int y = coordinate.getY();
 
+        state[x + y * 3] = state;
+    }
+
+    public int getCellState(Coordinate coordinate) {
+        int x = coordinate.getX();
+        int y = coordinate.getY();
+        return state[x + y * 3];
     }
 }

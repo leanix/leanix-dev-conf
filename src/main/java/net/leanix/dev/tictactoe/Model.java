3792c4e04079b98e21762221c78673c5bc672ee4
Dirk Lattermann <dirk.lattermann@leanix.net>
2018-12-19 10:31:09 +0100

    Create Model skeleton and Coordinate class

---
+++ src/main/java/net/leanix/dev/tictactoe/Model.java
@@ -0,0 +1,17 @@
+package net.leanix.dev.tictactoe;
+
+public class Model {
+
+    /**
+     *
+     * @param coordinate
+     * @param state 0 is empty, 1/2 is player number
+     */
+    public void setCellState(Coordinate coordinate, int state) {
+
+    }
+
+    public int getCellState(Coordinate coordinate) {
+
+    }
+}

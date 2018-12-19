6c1513e569d3f1a806b9f6fe00c6c32d223fc4eb
umutHatipoglu <umut.hatipoglu@gmail.com>
2018-12-19 12:21:16 +0100

    Implement render of board and attemptMove

--- main/java/net/leanix/dev/Board.java
+++ main/java/net/leanix/dev/Board.java
@@ -1,6 +1,7 @@
 package net.leanix.dev;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Board {
     CellState[][] cells = new CellState[3][3];
@@ -11,14 +12,27 @@ public class Board {
         }
     }
 
-    public boolean attemptMove(Player player, int x, int y) {
-        // validate coordinates
+    public boolean attemptMove(Player player, int row, int column) {
 
-        cells[x][y] = player.getCellState();
-        return true;
+        if (cells[row][column] == CellState.EMPTY) {
+            cells[row][column] = player.getCellState();
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public void render() {
+        String boardString = Arrays.stream(cells)
+            .map(this::toRowString)
+            .collect(Collectors.joining("-+-+-\n"));
+        System.out.println(boardString);
+    }
+
+    private String toRowString(CellState[] row) {
+        return Arrays.stream(row)
+            .map(CellState::toString)
+            .collect(Collectors.joining("|")) + "\n";
     }
 
     public GameState getUpdatedState() {
--- main/java/net/leanix/dev/CellState.java
+++ main/java/net/leanix/dev/CellState.java
@@ -1,5 +1,20 @@
 package net.leanix.dev;
 
 public enum CellState {
-    EMPTY
+    EMPTY,
+    X,
+    O;
+
+    @Override
+    public String toString() {
+        switch(this){
+            case O:
+                return "O";
+            case X:
+                return "X";
+            case EMPTY:
+            default:
+                return " ";
+        }
+    }
 }

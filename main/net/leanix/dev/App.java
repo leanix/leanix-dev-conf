3357e76031cee01c557b540ee91601203a00ae7f
Maik Schwan <maik.schwan@leanix.net>
2018-12-19 15:11:23 +0100

    parse args

--- main/net/leanix/dev/App.java
+++ main/net/leanix/dev/App.java
@@ -1,10 +1,26 @@
 package net.leanix.dev;
 
+import java.util.HashSet;
+import java.util.Set;
+import net.leanix.dev.Game.Player;
+
 public class App {
 
     public static void main(String[] args)
     {
-        Game game = new Game();
+        Set<Player> cpuPlayers = new HashSet<>();
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("-p1")) {
+                if ("cpu".equals(args[++i])) {
+                    cpuPlayers.add(Player.Player_1);
+                }
+            } else if(args[i].equals("-p2")) {
+                if ("cpu".equals(args[++i])) {
+                    cpuPlayers.add(Player.Player_2);
+                }
+            }
+        }
+        Game game = new Game(cpuPlayers);
         game.run();
     }
 }

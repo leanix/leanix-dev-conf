769eb16df03ecea00919f47224d819992a16d203
Rene Hamburger <rene.hamburger@leanix.net>
2018-12-19 23:02:18 +0100

    Make random CPU actually random

--- src/main/java/net/leanix/dev/tictactoe/RandomCpuPlayer.java
+++ src/main/java/net/leanix/dev/tictactoe/RandomCpuPlayer.java
@@ -1,5 +1,7 @@
 package net.leanix.dev.tictactoe;
 
+import java.util.Random;
+
 public class RandomCpuPlayer implements Player {
     private final int ownNumber;
     private final ConsoleOutput output;
@@ -10,15 +12,16 @@ public class RandomCpuPlayer implements Player {
     }
 
     public Coordinate submitMove(Board board) {
-        for (int x = 0; x < 3; x++) {
-            for (int y = 0; y < 3; y++) {
-                Coordinate coordinate = new Coordinate(x, y);
-                if (board.getCellState(coordinate) == 0) {
-                    output.printMessage("\nPlayer " + ownNumber + " selected " + (char)('A' + x) + y + "\n");
-                    return coordinate;
-                }
+        Random rand = new Random();
+        for (int i = 0; i < 1000; i++) {
+            int x = rand.nextInt(3);
+            int y = rand.nextInt(3);
+            Coordinate coordinate = new Coordinate(x, y);
+            if (board.getCellState(coordinate) == 0) {
+                output.printMessage("\nPlayer " + ownNumber + " selected " + (char) ('A' + x) + y + "\n");
+                return coordinate;
             }
         }
-        throw new RuntimeException("Board is full and no move is possible");
+        throw new RuntimeException("Unable to find a random field that is selectable. is the board full?");
     };
 }

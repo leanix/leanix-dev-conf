0f84ea250c9058627a76392f2a9488d326901ef7
Rene Hamburger <rene.hamburger@leanix.net>
2018-12-19 11:20:04 +0100

    Fix ConsoleOutput

--- src/main/java/net/leanix/dev/tictactoe/ConsoleOutput.java
+++ src/main/java/net/leanix/dev/tictactoe/ConsoleOutput.java
@@ -3,10 +3,15 @@ package net.leanix.dev.tictactoe;
 public class ConsoleOutput {
     public void printBoard(Board board) {
         for (int y = 2; y >= 0; y--) {
-            System.out.print(y + " | | ");
+            System.out.print(y);
             for (int x = 0; x < 3; x++) {
                 Coordinate coordinate = new Coordinate(x, y);
                 System.out.print(cellStateToCharacter(board.getCellState(coordinate)));
+                if (x < 2) {
+                    System.out.print("|");
+                } else {
+                    System.out.println();
+                }
             }
             System.out.println(" -+-+-");
         }
